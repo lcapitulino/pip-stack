d8b29c0584f5986ad19575df7a1f681d461a5832
Luiz Capitulino <lcapitulino@gmail.com>
2014-09-13 22:17:30 -0400

    check-ether: refactorings
    Signed-off-by: Luiz Capitulino <lcapitulino@gmail.com>

--- check-ether.c
+++ check-ether.c
@@ -19,46 +19,7 @@
 #include "common.h"
 #include "ether.h"
 
-START_TEST(test_addr_to_str)
-{
-	const uint8_t hwaddr[6] = { 0xf1, 0xf2, 0xf3, 0xf4, 0xf5, 0xf6 };
-	char str[18];
-
-	memset(str, 0, sizeof(str));
-	ether_addr_to_str(hwaddr, str, sizeof(str));
-	ck_assert_str_eq(str, "f1:f2:f3:f4:f5:f6");
-}
-END_TEST
-
-START_TEST(test_str_to_addr)
-{
-	const uint8_t hwaddr[6] = { 0xf1, 0xf2, 0xf3, 0xf4, 0xf5, 0xf6 };
-	const char str_ok[] = "f1:f2:f3:f4:f5:f6";
-	const char str_err1[] = "kjnfksnfskdds";
-	const char str_err2[] = "f1:f2";
-	const char str_err3[] = "f1:::::f2";
-	uint8_t hwaddr_tmp[6];
-	int ret;
-
-	memset(hwaddr_tmp, 0, sizeof(hwaddr_tmp));
-
-	ret = ether_str_to_addr(str_ok, hwaddr_tmp);
-	ck_assert_int_eq(ret, 0);
-	ck_assert_int_eq(hwaddr_eq(hwaddr_tmp, hwaddr), true);
-
-	/*
-	 * Test some errors
-	 */
-	ret = ether_str_to_addr(str_err1, hwaddr_tmp);
-	ck_assert_int_eq(ret, -1);
-
-	ret = ether_str_to_addr(str_err2, hwaddr_tmp);
-	ck_assert_int_eq(ret, -1);
-
-	ret = ether_str_to_addr(str_err3, hwaddr_tmp);
-	ck_assert_int_eq(ret, -1);
-}
-END_TEST
+const uint8_t hwaddr_gbl[6] = { 0xf1, 0xf2, 0xf3, 0xf4, 0xf5, 0xf6 };
 
 START_TEST(test_hwaddr_init)
 {
@@ -73,26 +34,62 @@ END_TEST
 
 START_TEST(test_hwaddr_eq)
 {
-	const uint8_t hwaddr1[6] = { 0xf1, 0xf2, 0xf3, 0xf4, 0xf5, 0xf6 };
-	const uint8_t hwaddr2[6] = { 0xf1, 0xf2, 0xf3, 0xf4, 0xf5, 0xf6 };
-	const uint8_t hwaddr3[6] = { 0xf1, 0xf2, 0xf3, 0xf5, 0xf5, 0xf6 };
+	const uint8_t hwaddr[6] = { 0xf1, 0xf2, 0xf3, 0xf5, 0xf5, 0xf6 };
 
-	ck_assert_int_eq(hwaddr_eq(hwaddr1, hwaddr2), true);
-	ck_assert_int_eq(hwaddr_eq(hwaddr1, hwaddr3), false);
+	ck_assert_int_eq(hwaddr_eq(hwaddr_gbl, hwaddr_gbl), true);
+	ck_assert_int_eq(hwaddr_eq(hwaddr_gbl, hwaddr), false);
 }
 END_TEST
 
 START_TEST(test_hwaddr_cp)
 {
-	const uint8_t hwaddr1[6] = { 0xf1, 0xf2, 0xf3, 0xf4, 0xf5, 0xf6 };
-	uint8_t hwaddr2[6];
+	uint8_t hwaddr[6];
 	int i;
 
-	hwaddr_init(hwaddr2, 0);
-	hwaddr_copy(hwaddr2, hwaddr1);
+	hwaddr_init(hwaddr, 0);
+	hwaddr_copy(hwaddr, hwaddr_gbl);
 
 	for (i = 0; i < 6; i++)
-		ck_assert_int_eq(hwaddr1[i], hwaddr2[i]);
+		ck_assert_int_eq(hwaddr_gbl[i], hwaddr[i]);
+}
+END_TEST
+
+START_TEST(test_addr_to_str)
+{
+	char str[18];
+
+	memset(str, 0, sizeof(str));
+	ether_addr_to_str(hwaddr_gbl, str, sizeof(str));
+	ck_assert_str_eq(str, "f1:f2:f3:f4:f5:f6");
+}
+END_TEST
+
+START_TEST(test_str_to_addr)
+{
+	const char str_ok[] = "f1:f2:f3:f4:f5:f6";
+	const char str_err1[] = "kjnfksnfskdds";
+	const char str_err2[] = "f1:f2";
+	const char str_err3[] = "f1:::::f2";
+	uint8_t hwaddr_tmp[6];
+	int ret;
+
+	memset(hwaddr_tmp, 0, sizeof(hwaddr_tmp));
+
+	ret = ether_str_to_addr(str_ok, hwaddr_tmp);
+	ck_assert_int_eq(ret, 0);
+	ck_assert_int_eq(hwaddr_eq(hwaddr_tmp, hwaddr_gbl), true);
+
+	/*
+	 * Test some errors
+	 */
+	ret = ether_str_to_addr(str_err1, hwaddr_tmp);
+	ck_assert_int_eq(ret, -1);
+
+	ret = ether_str_to_addr(str_err2, hwaddr_tmp);
+	ck_assert_int_eq(ret, -1);
+
+	ret = ether_str_to_addr(str_err3, hwaddr_tmp);
+	ck_assert_int_eq(ret, -1);
 }
 END_TEST
 
@@ -115,13 +112,12 @@ END_TEST
 
 START_TEST(test_dev_alloc_hwaddr)
 {
-	uint8_t hwaddr[6] = { 0xf1, 0xf2, 0xf3, 0xf4, 0xf5, 0xf6 };
 	struct ether_device *dev;
 
-	dev = ether_dev_alloc(hwaddr);
+	dev = ether_dev_alloc(hwaddr_gbl);
 	ck_assert(dev != NULL);
 
-	ck_assert_int_eq(hwaddr_eq(dev->hwaddr, hwaddr), true);
+	ck_assert_int_eq(hwaddr_eq(dev->hwaddr, hwaddr_gbl), true);
 
 	ether_dev_put(dev);
 }
@@ -157,10 +153,10 @@ END_TEST
 
 START_TEST(test_header_parsing_and_api)
 {
+	const uint8_t hwaddr_src[6] = { 0x1f, 0x2f, 0x3f, 0x4f, 0x5f, 0x6f };
+	const uint8_t *hwaddr_dst = hwaddr_gbl;
 	uint8_t buf[ETHER_FRAME_SIZE];
 	struct ether_frame *frame;
-	uint8_t hwaddr_dst[6] = { 0xf1, 0xf2, 0xf3, 0xf4, 0xf5, 0xf6 };
-	uint8_t hwaddr_src[6] = { 0x1f, 0x2f, 0x3f, 0x4f, 0x5f, 0x6f };
 	const uint8_t *p;
 	int i = 0;
 
@@ -229,11 +225,11 @@ Suite *ether_suite(void)
 	s = suite_create("Ethernet");
 
 	tc_misc = tcase_create("Misc");
-	tcase_add_test(tc_misc, test_addr_to_str);
-	tcase_add_test(tc_misc, test_str_to_addr);
 	tcase_add_test(tc_misc, test_hwaddr_init);
 	tcase_add_test(tc_misc, test_hwaddr_cp);
 	tcase_add_test(tc_misc, test_hwaddr_eq);
+	tcase_add_test(tc_misc, test_addr_to_str);
+	tcase_add_test(tc_misc, test_str_to_addr);
 	suite_add_tcase(s, tc_misc);
 
 	tc_frame = tcase_create("Frame");
